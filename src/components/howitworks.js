134e5c993757dba3c91413fb635f68d0ea6ee3a1
ggakila <gisorehaggai@gmail.com>
2023-11-23 23:52:35 +0300

    responsiveness

--- src/components/howitworks.js
+++ src/components/howitworks.js
@@ -2,15 +2,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 export default function Howitworks() {
   return (
-    <div className="container h-screen py-10 px-10 flex flex-col items-center justify-center gap-10">
+    <div className="container h-1/2 md:h-screen py-5 md:py-10 px-10 flex flex-col items-center justify-center gap-10">
       <div className="w-full flex flex-col items-center justify-center gap-6 ">
-        <h1 className="text-6xl w-2/3 text-center font-bold tracking-wide leading-normal">Generate Powerful AI Marketing Campaigns in Minutes</h1>
-        <p className="text-xl text-gray-600 font-medium text-center w-1/2">
+        <h1 className="text-3xl md:text-6xl w-2/3 text-center font-bold tracking-wide leading-normal">Generate Powerful AI Marketing Campaigns in Minutes</h1>
+        <p className="text-lg md:text-xl text-gray-600 font-medium text-center w-1/2">
             Our AI powered marketing campaign generator takes the hastle out of creating effective campaigns. 
             Simply input your business name, industry and customer data, and analyze your customer demographics to generate personalized campaigns that drive results
         </p>
       </div>
-      <div className="flex justify-between w-full">
+      <div className="flex flex-col md:flex-row items-center justify-between w-full">
       <div className="w-1/3 flex flex-col  gap-8 items-center justify-center p-6">
         <div className="relative w-12 h-12">
             <Image
@@ -21,7 +21,7 @@ export default function Howitworks() {
             />
         </div>
         <h2 className="text-4xl font-bold text-center">Input Business Details</h2>
-        <p className="text-lg text-center text-gray-600 font-medium">Simply input your business detailes, industry and customer demographics.</p>
+        <p className="text-md md:text-lg text-center text-gray-600 font-medium">Simply input your business detailes, industry and customer demographics.</p>
       </div>
       <div className="w-1/3 flex flex-col  gap-8 items-center justify-center p-6 ">
         <div className="relative w-12 h-12">
@@ -33,7 +33,7 @@ export default function Howitworks() {
             />
         </div>
         <h2 className="text-4xl font-bold text-center">Analyze Customer Demographics</h2>
-        <p className="text-lg text-center text-gray-600 font-medium">Our AI analyzes your customer demographics to understand their preferences and behavior</p>
+        <p className="text-md md:text-lg text-center text-gray-600 font-medium">Our AI analyzes your customer demographics to understand their preferences and behavior</p>
       </div>
       <div className="w-1/3 flex flex-col  gap-8 items-center justify-center p-6">
         <div className="relative w-12 h-12">
@@ -45,7 +45,7 @@ export default function Howitworks() {
             />
         </div>
         <h2 className="text-4xl font-bold text-center">Generate Personalized Campaigns</h2>
-        <p className="text-lg text-center text-gray-600 font-medium">Based on your business details, industry and customer demogrphocs, our AI generates personalized marketing campaigns tailored to your target audience.</p>
+        <p className="text-md md:text-lg text-center text-gray-600 font-medium">Based on your business details, industry and customer demogrphocs, our AI generates personalized marketing campaigns tailored to your target audience.</p>
       </div>
       </div>
       <Link href="/promptpage" className="bg-white text-black px-4 py-3 hover:text-white hover:bg-black border border-black font-semibold text-lg rounded">Get started</Link>
